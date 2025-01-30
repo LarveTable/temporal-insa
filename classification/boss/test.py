e4f26947c0c6d7ed42756a7c75686484bfde77d5
LarveTable <cyorick@gmail.com>
2025-01-30 01:01:02 +0100

    boss works

--- classification/boss/test.py
+++ classification/boss/test.py
@@ -1,7 +1,5 @@
-from minirocket import transform, fit
-from sklearn.linear_model import RidgeClassifierCV
-from sklearn.preprocessing import StandardScaler
-from sklearn.pipeline import make_pipeline
+from pyts.transformation import BOSS
+from sklearn.svm import SVC
 import time
 
 # load data from UCR archive
@@ -10,42 +8,39 @@ import numpy as np
 
 print(f"Loading data".ljust(80 - 5, "."), end = "", flush = True)
 
-training_data = np.loadtxt("UCRArchive_2018/Adiac/Adiac_TRAIN.tsv")
+training_data = np.loadtxt("UCRArchive_2018/BME/BME_TRAIN.tsv")
 cleaned_data = np.nan_to_num(training_data[:, 1:], nan=0.0).astype(np.float32) #Cleaned data from NaN values to 0.0
 Y_training, X_training = training_data[:, 0].astype(np.int32), cleaned_data
 
-test_data = np.loadtxt("UCRArchive_2018/Adiac/Adiac_TEST.tsv")
+test_data = np.loadtxt("UCRArchive_2018/BME/BME_TEST.tsv")
 cleaned_data = np.nan_to_num(test_data[:, 1:], nan=0.0).astype(np.float32) #Cleaned data from NaN values to 0.0
 Y_test, X_test = test_data[:, 0].astype(np.int32), cleaned_data
 
 print("Done.")
 
-# note:
-# * input time series do *not* need to be normalised
-# * input data should be np.float32
+# Création du transformateur BOSS
+boss = BOSS(word_size=4, n_bins=4, window_size=32, sparse=True)
 
 time_a_trans = time.perf_counter()
-parameters = fit(X_training)
-X_training_transform = transform(X_training, parameters)
+X_training_transform = boss.fit_transform(X_training).toarray()
 time_b_trans = time.perf_counter()
 time_trans = time_b_trans - time_a_trans
 
 time_a_trans = time.perf_counter()
-X_test_transform = transform(X_test, parameters)
+X_test_transform = boss.transform(X_test).toarray()
 time_b_trans = time.perf_counter()
 time_trans_test = time_b_trans - time_a_trans
 
+
 time_a = time.perf_counter()
-model = make_pipeline(
-    StandardScaler(with_mean=False), RidgeClassifierCV(alphas = np.logspace(-3, 3, 10)))
-model.fit(X_training_transform, Y_training)
+# SVM
+classifier = SVC()
+classifier.fit(X_training_transform, Y_training)
 time_b = time.perf_counter()
 time_training = time_b - time_a
 
-#predictions = model.predict(X_test_transform)
-
 time_a = time.perf_counter()
-score = model.score(X_test_transform, Y_test)
+score = classifier.score(X_test_transform, Y_test)
 time_b = time.perf_counter()
 time_testing = time_b - time_a
 
